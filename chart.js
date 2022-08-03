973d62d7b731803b6c91f9af7c5a8a184ce4d950
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-08-03 11:46:14 +0100

    use credits

--- chart.js
+++ chart.js
@@ -3,14 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
         chart: {
             // type: 'column'
             // type:'pie'
-            type:'line'
+            // type:'line'
             // type:'area'
             //  type:'scatter'
             //  type:'spline'
-            //   type:'areaspline'
+              type:'areaspline'
             //   type:'bar'
             //    type:'arearange'
             //    type:'columnrange'
+        },
+        cedits: {
+            enabled: true
         },
 
         title: {
@@ -26,12 +29,17 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         series: [{
             name: 'Jane',
-            data: [1, 0, 4]
+            data: [1, 0, 4,30,43,23,1,2,34]
             
         }, {
             name: 'John',
-            data: [5, 7, 3]
-        }]
+            data: [5, 7, 3,2,1,20,4,3,2]
+        },
+        {
+            name: 'Abdellah',
+            data: [5, 7, 3,2,1,20,4,3,2]
+        }],
+        
     });
 
 });
